6954594717e6fb6f04f6c7f4d529eb6b2f6f43b3
ldheeraj18 <abdheeraj18@gmail.com>
2022-06-21 17:26:17 +0530

    second commit

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express();
 const path = require('path');
 const { v4: uuid } = require('uuid');
+const methodOveride = require('method-override');
 uuid();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOveride('_method'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -50,7 +52,25 @@ app.get('/comments/:id', (req, res) => {
     res.render('comments/show', { comment });
 })
 
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment });
+})
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newcomment = req.body.comment;
+    const findcomment = comments.find(c => c.id === id);
+    findcomment.comment = newcomment;
+    res.redirect('/comments');
+})
 
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const foundcomment = comments.find(c => c.id !== id);
+    comments = comments.filter(foundcomment);
+    res.redirect('/comments');
+})
 app.listen(5000, () => {
     console.log("On port 5000");
 })
